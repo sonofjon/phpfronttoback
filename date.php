04c01dd015c942eddb20e228f00e75a241860abf
Andreas Jonsson <andreasjon@gmail.com>
2021-02-04 14:21:37 +0100

    Date/time string and strtotime()

--- date.php
+++ date.php
@@ -6,6 +6,14 @@
 	echo date('h:i:sa');
 	*/
 
+	/*
 	$timestamp = mktime(10, 14, 54, 9, 10, 1981); 
 	echo date('Y/m/d h:i:sa', $timestamp);
+	*/
+
+	$timestamp2 = strtotime('7:00pm March 22 2016');
+	$timestamp3 = strtotime('tomorrow');
+	$timestamp4 = strtotime('next Sunday');
+	$timestamp5 = strtotime('+2 Days');
+	echo date('Y/m/d h:i:sa', $timestamp5);
 ?>
