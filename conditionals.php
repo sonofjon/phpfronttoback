f450e6c0681345a6dbcc94646dcd706754799729
Andreas Jonsson <andreasjon@gmail.com>
2021-02-04 12:35:24 +0100

    Logical operators

--- conditionals.php
+++ conditionals.php
@@ -1,4 +1,5 @@
 <?php 
+	/*
 	$num = 4;
 	if ($num == 5) {
 		echo "5 passed";
@@ -6,5 +7,20 @@
 		echo "6 passed";
 	} else {
 		echo "Did not pass";
+	}
+	*/
+
+	/*
+	$num = 6;
+	if ($num > 5) {
+		if ($num < 10) {
+			echo "$num passed";
+		}
+	}
+	*/
+
+	$num = 5;
+	if ($num > 5 && $num < 10) {
+		echo "$num passed";
 	}
 ?>
