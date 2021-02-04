35c77f8de8238fe27f95a2d858ee1395a80c7074
Andreas Jonsson <andreasjon@gmail.com>
2021-02-04 12:41:55 +0100

    switch()

--- conditionals.php
+++ conditionals.php
@@ -19,8 +19,26 @@
 	}
 	*/
 
+	/*
 	$num = 5;
 	if ($num > 5 && $num < 10) {
 		echo "$num passed";
+	}
+	*/
+
+	$favVolor = 'yellow';
+	switch ($favVolor) {
+		case 'red':
+			echo 'Your favorite color is red';
+			break;
+		case 'blue':
+			echo 'Your favorite color is blue';
+			break;
+		case 'green':
+			echo 'Your favorite color is green';
+			break;	
+		default:
+			echo 'Your favorite color is something else';
+			#break;
 	}
 ?>
