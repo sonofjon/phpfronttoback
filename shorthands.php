65c1591278560c3794c8be06cdd56beaa14fde2c
Andreas Jonsson <andreasjon@gmail.com>
2021-03-05 16:12:51 +0100

    Shorthand syntax for loops: if, foreach, and for

--- shorthands.php
+++ shorthands.php
@@ -1,6 +1,6 @@
 <?php 
 	$loggedIn = true;
-
+	$arr = [1,2,3,4,5];
 	/*
 	if ($loggedIn) {
 		echo "You are logged in";
@@ -16,11 +16,43 @@
 	#$isRegistered = ($loggedIn == true) ? true : false;
 	#echo $isRegistered;
 
+/*
 	# Nested ternary operator statements
 	$age = 5;
 	$score = 5;
 
 	echo 'Your score is ' . ($score > 10 ? ($age > 10 ? 'Average' : 'Excellent') : ($age > 10 ? 'Horrible' : 'Average'));
-
+*/
 
  ?>
+
+ <div>
+ 	<?php if ($loggedIn) { ?>
+ 		<h1>Welcome User</h1>
+	<?php } else { ?>
+ 		<h1>Welcome Guest</h1>
+	<?php } ?>
+ </div>
+
+<!-- Shorthand if-statement -->
+<div>
+ 	<?php if ($loggedIn): ?>
+ 		<h1>Welcome User</h1>
+	<?php else: ?>
+ 		<h1>Welcome Guest</h1>
+	<?php endif; ?>
+ </div>
+
+<!-- Shorthand foreach-statement -->
+<div>
+ 	<?php foreach ($arr as $val): ?>
+ 		<?php echo $val ?>
+	<?php endforeach; ?>
+ </div>
+
+<!-- Shorthand for-statement -->
+<div>
+ 	<?php for ($i=0; $i<10 ; $i++): ?>
+ 		<li><?php echo $i ?></li>
+	<?php endfor; ?>
+ </div>
