4491d478c4b0aa9cfbcc11e96f2b316644a64e3e
Andreas Jonsson <andreasjon@gmail.com>
2021-03-08 16:06:14 +0100

    Filter input array

--- filters.php
+++ filters.php
@@ -31,15 +31,29 @@
 	}
 
 	echo filter_var($var, FILTER_SANITIZE_NUMBER_INT);
-	*/
 
 	$var = "<script>alert(1)</script>";
 	# echo $var;
 	echo filter_var($var, FILTER_SANITIZE_SPECIAL_CHARS);
+	*/
+
+	$filters = array(
+		"data" => FILTER_VALIDATE_EMAIL,
+		"data2" => array(
+			"filter" => FILTER_VALIDATE_INT,
+			"options" => array(
+				"min_range" => 1,
+				"max_range" => 100
+			)
+		)
+	);
+
+	print_r(filter_input_array(INPUT_POST, $filters));
  ?>
 
  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
  	<!-- PHP_SELF points to filename of the currently executing script -->
  	<input type="text" name="data">
+ 	<input type="text" name="data2">
  	<button type="submit">Submit</button>
  </form>
